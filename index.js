e90948c6026baa14bbdb1a265d0b6fc61e57532f
Abraham De Sodano Dorsainvil <dorsainvilamswonabrahamdesodan@gmail.com>
2024-07-27 21:46:49 +0400

    My Website Project about Football that's the name is InfoClubSport

---
+++ index.js
@@ -0,0 +1,17 @@
+
+document.getElementById('myReal').addEventListener('click',function () {
+    alert('Cristiano Ronaldo')
+}
+)
+
+//For Inter Milano
+document.getElementById('myInter').addEventListener('click',function () {
+    alert('Giuseppe Meazza')
+}
+)
+
+// For Bayern Munich
+document.getElementById('myBayern').addEventListener('click',function () {
+    alert('Gerd Müller')
+}
+)
